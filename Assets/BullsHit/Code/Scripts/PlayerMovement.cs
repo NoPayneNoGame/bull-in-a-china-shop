6456cb5d5aec33cb0bca4998b58c99614587fdeb
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-11-06 03:28:35 +1300

    Added basic movement and stun mechanic on wall collision

--- Assets/BullsHit/Code/Scripts/PlayerMovement.cs
+++ Assets/BullsHit/Code/Scripts/PlayerMovement.cs
@@ -3,11 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+
+  public Rigidbody rb;
+  public float moveSpeed = 20;
+  public float maxSpeed = 30;
+  public float drag = 0.98f;
+  public float rotationSpeed = 10;
+
+  private Vector3 moveForce;
+  private bool canMove = true;
+
+  void enableMovement() {
+    canMove = true;
+    // This is a temp fix for the player getting stuck in a loop after hitting a wall and then not having a vertical input / change in rotation
+    moveForce = new Vector3(0, 0, 0);
+  }
+
+  void OnCollisionEnter(Collision hit) {
+    if (hit.gameObject.tag == "Wall") {
+      canMove = false;
+      Invoke("enableMovement", 1.0f);
+    }
+  }
+
   void Start() {
-    
+
   }
 
   void Update() {
-    
+    if (canMove) {
+      float horizontal = Input.GetAxisRaw("Horizontal");
+      float vertical = Input.GetAxisRaw("Vertical");
+      if (horizontal != 0) {
+        rb.transform.Rotate(Vector3.up * horizontal * moveForce.magnitude * rotationSpeed * Time.deltaTime);
+      }
+      // Not sure if using velocity is better here but this is easier for now
+      moveForce += rb.transform.forward * moveSpeed * Time.deltaTime * vertical * drag;
+      moveForce = Vector3.ClampMagnitude(moveForce, maxSpeed);
+      rb.transform.position += moveForce * Time.deltaTime;
+    } else {
+      // Not a very realistic bounce when wall is approached at an angle
+      rb.transform.position += -moveForce / 2 * Time.deltaTime;
+    }
   }
 }
