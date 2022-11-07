6f4f442ea6d916f62e199be6b6ac154dc90dd989
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-11-08 06:44:08 +1300

    Added basic npc patrolling and collision using NavMesh and NavMeshAgent components

---
+++ Assets/BullsHit/Code/Scripts/EnemyPatrol.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+
+public class EnemyPatrol : MonoBehaviour {
+
+  public Transform[] points;
+  private int destPoint = 0;
+  private NavMeshAgent agent;
+
+
+  void Start() {
+    agent = GetComponent<NavMeshAgent>();
+    agent.autoBraking = false;
+
+    GotoNextPoint();
+  }
+
+
+  void GotoNextPoint() {
+    if (points.Length == 0)
+      return;
+
+    agent.destination = points[destPoint].position;
+    destPoint = (destPoint + 1) % points.Length;
+  }
+
+
+  void Update() {
+    if (!agent.pathPending && agent.remainingDistance < 0.5f)
+      GotoNextPoint();
+  }
+}
