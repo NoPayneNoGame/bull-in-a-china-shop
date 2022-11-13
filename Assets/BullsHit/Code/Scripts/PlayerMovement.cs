14e4a2a5dff3838a86bb299cfbc3c491a23633a0
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-11-13 16:36:56 +1300

    Added jumping, trying to fix camera zoom

--- Assets/BullsHit/Code/Scripts/PlayerMovement.cs
+++ Assets/BullsHit/Code/Scripts/PlayerMovement.cs
@@ -5,13 +5,11 @@ public class PlayerMovement : MonoBehaviour {
 
   public Rigidbody rb;
   public float moveSpeed = 20;
-  public float maxSpeed = 100;
   public float drag = 0.98f;
   public float rotationSpeed = 10;
   public float bonkThreshold = 500;
   public float bonkPower = 10; // The max distance (in metres?) between player and bonkable object where the object will fall
   public float jumpHeight = 10;
-  public float jumpDuration = 0.5f;
   public float fallSpeed = 10;
 
   [Header("Camera Things")]
@@ -28,8 +26,7 @@ public class PlayerMovement : MonoBehaviour {
   private float currentSpeed;
   private bool canMove = true;
   private bool canJump = true;
-  private bool isJumping = false;
-  private bool isFalling = false;
+  private bool shouldJump = false;
 
   private CinemachineTransposer transposer;
 
@@ -62,24 +59,17 @@ public class PlayerMovement : MonoBehaviour {
     checkBonkables(rb.position);
   }
 
-  // void jump() {
-  //   isJumping = true;
-  //   canJump = false;
-  //   Invoke("endJump", jumpDuration);
-  // }
 
-  // void endJump() {
-  //   isJumping = false;
-  //   isFalling = true;
-  // }
 
   void OnCollisionEnter(Collision hit) {
     if ((hit.gameObject.tag == "Enemy" || (hit.gameObject.tag == "Wall")) && currentSpeed > bonkThreshold) {
       bonk(hit);
     }
+
+  }
+
+  void OnCollisionStay(Collision hit) {
     if (hit.gameObject.tag == "Floor") {
-      isFalling = false;
-      isJumping = false;
       canJump = true;
     }
   }
@@ -98,19 +88,28 @@ public class PlayerMovement : MonoBehaviour {
         moveForce = rb.transform.forward * vertical * moveSpeed;
         rb.AddForce(moveForce);
       }
+      if (shouldJump && canJump) {
+        rb.AddForce(rb.transform.up * jumpHeight, ForceMode.Impulse);
+        canJump = false;
+      }
     }
 
-    // For monitoring speed
-    currentSpeed = Vector3.Distance(prevPosition, rb.transform.position);
-    prevPosition = rb.transform.position;
+
   }
 
   void Update() {
 
     horizontal = Input.GetAxisRaw("Horizontal");
     vertical = Input.GetAxisRaw("Vertical");
+    shouldJump = Input.GetButton("Jump");
 
-    if (moveForce.magnitude > ((maxSpeed - moveSpeed) / 2)) {
+    // For monitoring speed
+    currentSpeed = Vector3.Distance(prevPosition, rb.transform.position) * 100;
+    prevPosition = rb.transform.position;
+
+    Debug.Log(currentSpeed);
+    Debug.Log(moveSpeed / 1000);
+    if (currentSpeed > moveSpeed / 1000) {
       transposer.m_FollowOffset.y = Mathf.Lerp(transposer.m_FollowOffset.y, maxCameraDistance, zoomOutSpeed * Time.deltaTime);
     } else {
       transposer.m_FollowOffset.y = Mathf.Lerp(transposer.m_FollowOffset.y, minCameraDistance, zoomInSpeed * Time.deltaTime);
