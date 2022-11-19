b4d9b849cf3e7910fa6b8089bd96593e3922d75e
Steven Payne <steven.g.payne@hotmail.com>
2022-11-19 22:56:55 +1000

    set various bools to do various animations

--- Assets/BullsHit/Code/Scripts/PlayerMovement.cs
+++ Assets/BullsHit/Code/Scripts/PlayerMovement.cs
@@ -4,17 +4,30 @@ using Cinemachine;
 public class PlayerMovement : MonoBehaviour {
 
   public Rigidbody rb;
+  public Animator animator;
+
   public float moveSpeed = 20;
   public float drag = 0.98f;
   public float rotationSpeed = 10;
-  public float bonkThreshold = 500;
+  public float bonkThreshold = 0.2f;
+
+  [Tooltip("The max distance between player and bonkable object where the object will fall.")]
   public float bonkPower = 10; // The max distance (in metres?) between player and bonkable object where the object will fall
   public float jumpHeight = 10;
   public float fallSpeed = 10;
+
+  [Tooltip("Seconds to go from current speed to nearly zero.")]
+  public float breakTime = 0.2f; // seconds
+  [Tooltip("Minimum speed to stop the breaking animation and return to run/idle even if still holding the break key.")]
+  public float minSpeedForBreakAnim = 2;
+  [Tooltip("Seconds to remain stunned after bonking.")]
+  public float stunDuration = 1f; // seconds
+
   [SerializeField] private GameObject dashLight;
   [SerializeField] private float dashMultiplier = 50f;
   [SerializeField] private float dashCooldown = 1f;
   [SerializeField] private float dashDuration = 0.2f;
+
 
   [Header("Camera Things")]
   public CinemachineVirtualCamera vcam;
@@ -39,6 +52,7 @@ public class PlayerMovement : MonoBehaviour {
 
   void enableMovement() {
     canMove = true;
+    animator.SetBool("WallCollision", false);
     // This is a temp fix for the player getting stuck in a loop after hitting a wall and then not having a vertical input / change in rotation
     //moveForce = new Vector3(0, 0, 0);
   }
@@ -50,11 +64,12 @@ public class PlayerMovement : MonoBehaviour {
   void endDash() {
     shouldDash = false;
     dashLight.SetActive(false);
+    animator.SetBool("Dashing", false);
   }
 
   void unfreezeBonkable(GameObject bonkable) {
-    Rigidbody bonkrb = bonkable.GetComponent<Rigidbody>();
-    bonkrb.constraints = RigidbodyConstraints.None;
+    Rigidbody bonkRb = bonkable.GetComponent<Rigidbody>();
+    bonkRb.constraints = RigidbodyConstraints.None;
   }
 
   void checkBonkables(Vector3 position) {
@@ -68,7 +83,8 @@ public class PlayerMovement : MonoBehaviour {
 
   void bonk(Collision hit) {
     canMove = false;
-    Invoke("enableMovement", 1.0f);
+    animator.SetBool("WallCollision", true);
+    Invoke("enableMovement", stunDuration);
     Vector3 reflected = Vector3.Reflect(moveForce, hit.contacts[0].normal) / 2;
     rb.velocity = Vector3.zero;
     rb.AddForce(reflected, ForceMode.Impulse);
@@ -84,11 +100,15 @@ public class PlayerMovement : MonoBehaviour {
   void OnCollisionStay(Collision hit) {
     if (hit.gameObject.tag == "Floor") {
       canJump = true;
+      animator.SetBool("Jumping", false);
     }
   }
 
   void Start() {
     transposer = vcam.GetCinemachineComponent<CinemachineTransposer>();
+    if (animator == null) {
+      animator = GetComponentInChildren<Animator>();
+    }
   }
 
   void FixedUpdate() {
@@ -97,15 +117,20 @@ public class PlayerMovement : MonoBehaviour {
         // Remove rb.velocity.magnitude if we want rotation while not moving
         rb.transform.Rotate(Vector3.up * horizontal * rotationSpeed * rb.velocity.magnitude * Time.deltaTime);
       }
+
       if (vertical != 0 && !shouldDash) {
         moveForce = rb.transform.forward * vertical * moveSpeed;
         rb.AddForce(moveForce);
       }
+
       if (shouldJump && canJump) {
+        animator.SetBool("Jumping", true);
         rb.AddForce(rb.transform.up * jumpHeight, ForceMode.Impulse);
         canJump = false;
       }
+
       if (shouldDash && canDash) {
+        animator.SetBool("Dashing", true);
         rb.velocity = Vector3.zero;
         float dashDirection = vertical != 0 ? vertical : 1;
         rb.AddForce(moveSpeed * dashMultiplier * dashDirection * rb.transform.forward);
@@ -115,19 +140,24 @@ public class PlayerMovement : MonoBehaviour {
         Invoke("endDash", dashDuration);
         Invoke("enableDash", dashCooldown);
       }
+
       if (shouldBrake) {
-        rb.velocity = rb.velocity * 0.9f;
+        rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, Time.deltaTime * (1 / breakTime));
+        animator.SetBool("Breaking", rb.velocity.magnitude >= minSpeedForBreakAnim); // stop the breaking anim once slow enough
+      } else {
+        animator.SetBool("Breaking", false);
       }
     }
   }
 
   void Update() {
-
     horizontal = Input.GetAxisRaw("Horizontal");
     vertical = Input.GetAxisRaw("Vertical");
     shouldJump = Input.GetButton("Jump");
     shouldDash = Input.GetButton("Dash");
     shouldBrake = Input.GetButton("Brake");
+
+    animator.SetBool("Running", vertical != 0);
 
     // For monitoring speed
     if (prevPosition != rb.transform.position) {
