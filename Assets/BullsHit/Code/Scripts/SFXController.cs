270e55d81eb1381868239708a1f96449f683c2d2
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-12-04 10:45:00 +1300

    Added volume slider for sound effects

---
+++ Assets/BullsHit/Code/Scripts/SFXController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour {
+
+  private GameObject sounds;
+  private AudioSource[] audioSources;
+  private float[] initialVolumes;
+
+  void Start() {
+    sounds = GameObject.FindGameObjectWithTag("SFX");
+    audioSources = sounds.GetComponentsInChildren<AudioSource>();
+    initialVolumes = getInitialVolumes();
+  }
+
+  private float[] getInitialVolumes() {
+    float[] volumes = new float[audioSources.Length];
+    for (int i = 0; i < audioSources.Length; i++) {
+      volumes[i] = audioSources[i].volume;
+    }
+    return volumes;
+  }
+
+  public void adjustVolumeOfAllSounds(float volumePercentage) {
+    for (int i = 0; i < audioSources.Length; i++) {
+      audioSources[i].volume = volumePercentage * initialVolumes[i];
+    }
+  }
+}
